247872b522b11ecf522e0639069eb9856071e511
Sofia <szamora@miuandes.cl>
2025-04-17 12:28:34 -0400

    lab 6 dooone

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
+//= link controllers/index.js
+//= link controllers/application.js
+//= link_tree ../../javascript/controllers .js
+
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,4 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/dist/esm/index.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "controllers", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
